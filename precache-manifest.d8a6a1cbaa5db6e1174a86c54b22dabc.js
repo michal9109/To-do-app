c5c735939182c6fa7f6c33ea3650ed5e225468c1
Dominika Dyrcz-Ciesielska <ciesielski.michal@interia.eu>
2020-07-04 00:59:45 +0200

    Updates

---
+++ precache-manifest.d8a6a1cbaa5db6e1174a86c54b22dabc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fef193739f597f03b4f9aba5f8891c",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "d171e60777f1965fdfda",
+    "url": "/To-do-app/static/css/main.daec47f1.chunk.css"
+  },
+  {
+    "revision": "6b59b1b18da9c671221b",
+    "url": "/To-do-app/static/js/2.80284a23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.80284a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d171e60777f1965fdfda",
+    "url": "/To-do-app/static/js/main.d4d23815.chunk.js"
+  },
+  {
+    "revision": "428058c55759962ddeab",
+    "url": "/To-do-app/static/js/runtime-main.f943e17a.js"
+  }
+]);
